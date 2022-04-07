3c18ad87692fd2d3cbb40d7f949177120155b9f7
senspond20 <senspond@gmail.com>
2022-04-07 23:42:34 +0900

    calculatorInterpreter, test

--- src/main/java/com/senspond/exp/interpreter/CalculatorInterpreter.java
+++ src/main/java/com/senspond/exp/interpreter/CalculatorInterpreter.java
@@ -1,8 +1,39 @@
 package com.senspond.exp.interpreter;
 import com.senspond.exp.alntlr.CalculatorBaseVisitor;
+import com.senspond.exp.alntlr.CalculatorParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class CalculatorInterpreter extends CalculatorBaseVisitor {
-    public CalculatorInterpreter(){
+public class CalculatorInterpreter extends CalculatorBaseVisitor<Double> {
+    private static final Logger log = LoggerFactory.getLogger(CalculatorInterpreter.class);
+    public CalculatorInterpreter(){}
+
+    @Override
+    public Double visitParens(CalculatorParser.ParensContext ctx) {
+        return visit(ctx.expression());
     }
 
+    @Override
+    public Double visitNum(CalculatorParser.NumContext ctx) {
+        Double num = Double.parseDouble(ctx.getText());
+        return num;
+    }
+
+    @Override
+    public Double visitAddSub(CalculatorParser.AddSubContext ctx) {
+        Double left =   Double.parseDouble(visit(ctx.expression(0)).toString());
+        Double right =  Double.parseDouble(visit(ctx.expression(1)).toString());
+        Double result = ctx.op.getType() == CalculatorParser.ADD ? left + right : left - right;
+//        log.debug("{} {} {} {} = {}", ctx.op.getType() == CalculatorParser.ADD ? "더하기" : "빼기", left, ctx.op.getText(), right, result);
+        return result;
+    }
+
+    @Override
+    public Double visitMulDiv(CalculatorParser.MulDivContext ctx) {
+        Double left = Double.parseDouble(visit(ctx.expression(0)).toString());
+        Double right =  Double.parseDouble(visit(ctx.expression(1)).toString());
+        Double result = ctx.op.getType() == CalculatorParser.MUL ? left * right : left / right;
+//        log.debug("{} {} {} {} = {}", ctx.op.getType() == CalculatorParser.MUL ? "곱하기" : "나누기", left, ctx.op.getText(), right, result);
+        return result;
+    }
 }
--- src/test/java/com/senspond/exp/antlr/CalculatorTests.java
+++ src/test/java/com/senspond/exp/antlr/CalculatorTests.java
@@ -1,6 +1,7 @@
 package com.senspond.exp.antlr;
 import com.senspond.exp.alntlr.CalculatorLexer;
 import com.senspond.exp.alntlr.CalculatorParser;
+import com.senspond.exp.interpreter.CalculatorInterpreter;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -15,11 +16,35 @@ public class CalculatorTests {
         CalculatorParser parser = new CalculatorParser(tokens);
         return parser;
     }
-
-    @Test
-    public void test(){
-        CalculatorParser parser = getParserTreeFromExp("21+2-4*2");
+    private static Double calculator(String exp){
+        CalculatorParser parser = getParserTreeFromExp(exp);
         ParseTree tree = parser.expression();
-        System.out.println(tree.toStringTree(parser));
+        Double query = new CalculatorInterpreter().visit(tree);
+        return query;
+    }
+    @Test
+    public void test1(){
+        String exp = "21+2-4*2";
+        System.out.println(exp + " = " + calculator(exp));
+    }
+    @Test
+    public void test2(){
+        String exp = "21+(2-4)*2";
+        System.out.println(exp + " = " + calculator(exp));
+    }
+    @Test
+    public void test3(){
+        String exp = "6/2-4";
+        System.out.println(exp + " = " + calculator(exp));
+    }
+    @Test
+    public void test4(){
+        String exp = "5*(2+3)/4";
+        System.out.println(exp + " = " + calculator(exp));
+    }
+    @Test
+    public void test5(){
+        String exp = "5*(2+3)/4 + 5-3*10-(3+5)";
+        System.out.println(exp + " = " + calculator(exp));
     }
 }
