6e046d72082b3e060d690bc65e52e62731dc6a44
senspond20 <senspond@gmail.com>
2022-04-09 17:34:44 +0900

    CalculatorInterpreter - remove unused import

--- src/main/java/com/senspond/exp/interpreter/CalculatorInterpreter.java
+++ src/main/java/com/senspond/exp/interpreter/CalculatorInterpreter.java
@@ -7,9 +7,7 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Getter
